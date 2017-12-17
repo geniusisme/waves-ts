c47ae842ddf17a68b7e3c7f172a67a46acc744e4
Eugeen Sablin <EugeenSablin@gmail.com>
2017-12-17 17:51:11 +0300

    factor out Application class + listen to context lost

--- src/main.ts
+++ src/main.ts
@@ -3,69 +3,82 @@ import {DetailedSquare} from "./detailed_square";
 import * as vs from './vs.glsl';
 import * as fs from './fs.glsl';
 
-class Context {
-    canvas: any;
-    plane: DetailedSquare;
-    vertex_buffer: any;
-    index_buffer: any;
-    coord_attrib: any;
+window.onload = () => {
+    let app = new Application(document.getElementById('my_Canvas'));
 }
 
-function main() {
-    let context = new Context();
-
-    context.canvas = document.getElementById('my_Canvas');
-    let gl = context.canvas.getContext('experimental-webgl');
-
-    context.plane = new DetailedSquare(0.3, 100);
-
-    context.vertex_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, context.vertex_buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, context.plane.vertices, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ARRAY_BUFFER, null);
-
-    context.index_buffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, context.index_buffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, context.plane.indices, gl.STATIC_DRAW);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
-
-    let vertShader = gl.createShader(gl.VERTEX_SHADER);
-    gl.shaderSource(vertShader, vs);
-    gl.compileShader(vertShader);
-
-    let fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-    gl.shaderSource(fragShader, fs);
-    gl.compileShader(fragShader);
-
-    let shaderProgram = gl.createProgram();
-    gl.attachShader(shaderProgram, vertShader);
-    gl.attachShader(shaderProgram, fragShader);
-    gl.linkProgram(shaderProgram);
-    gl.useProgram(shaderProgram);
-
-    context.coord_attrib = gl.getAttribLocation(shaderProgram, "coordinates");
-
-    gl.viewport(0,0,context.canvas.width,context.canvas.height);
-
-    start_animation(gl, context);
-}
-
-window.onload = main;
-
-function start_animation(gl: any, context: Context) {
-
-    function draw_frame() {
-        gl.bindBuffer(gl.ARRAY_BUFFER, context.vertex_buffer);
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, context.index_buffer);
-        gl.vertexAttribPointer(context.coord_attrib, 3, gl.FLOAT, false, 0, 0);
-        gl.enableVertexAttribArray(context.coord_attrib);
-        gl.clearColor(0.1, 0.5, 0.1, 1.0);
-        gl.clear(gl.COLOR_BUFFER_BIT);
-
-        gl.drawElements(gl.TRIANGLES, context.plane.indices.length, gl.UNSIGNED_SHORT,0);
-
-        requestAnimationFrame(draw_frame);
+class Application {
+    constructor(canvas: any) {
+        this.canvas = canvas;
+        this.water = new DetailedSquare(0.3, 100);
+        this.setup_gl_resources();
+        this.start_rendering();
+        this.setup_listeners();
     }
 
-    draw_frame();
+    private setup_gl_resources() {
+        this.gl = this.canvas.getContext('webgl');
+
+        this.vertex_buffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_buffer);
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, this.water.vertices, this.gl.DYNAMIC_DRAW);
+
+        this.index_buffer = this.gl.createBuffer();
+        this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+        this.gl.bufferData(this.gl.ELEMENT_ARRAY_BUFFER, this.water.indices, this.gl.STATIC_DRAW);
+
+        let vertShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+        this.gl.shaderSource(vertShader, vs);
+        this.gl.compileShader(vertShader);
+
+        let fragShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+        this.gl.shaderSource(fragShader, fs);
+        this.gl.compileShader(fragShader);
+
+        let shaderProgram = this.gl.createProgram();
+        this.gl.attachShader(shaderProgram, vertShader);
+        this.gl.attachShader(shaderProgram, fragShader);
+        this.gl.linkProgram(shaderProgram);
+        this.gl.useProgram(shaderProgram);
+
+        this.coord_attrib = this.gl.getAttribLocation(shaderProgram, "coordinates");
+
+        this.gl.viewport(0, 0, this.gl.drawingBufferWidth, this.gl.drawingBufferHeight);
+    }
+
+    private start_rendering() {
+        let render = () => {
+            this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertex_buffer);
+            this.gl.bindBuffer(this.gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+            this.gl.vertexAttribPointer(this.coord_attrib, 3, this.gl.FLOAT, false, 0, 0);
+            this.gl.enableVertexAttribArray(this.coord_attrib);
+            this.gl.clearColor(0.3, 0.1, 0.1, 1.0);
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+
+            this.gl.drawElements(this.gl.TRIANGLES, this.water.indices.length, this.gl.UNSIGNED_SHORT, 0);
+
+            this.frameRequest = requestAnimationFrame(render);
+        }
+        render();
+    }
+
+    private setup_listeners() {
+        this.canvas.addEventListener("webglcontextlost", (event: any) => {
+            event.preventDefault();
+            window.cancelAnimationFrame(this.frameRequest);
+        }, false);
+
+        this.canvas.addEventListener("webglcontextrestored", (event: any) => {
+            this.setup_gl_resources();
+            this.start_rendering();
+        }, false);
+    }
+
+    private canvas: any;
+    private vertex_buffer: any;
+    private index_buffer: any;
+    private coord_attrib: any;
+    private gl: any;
+    private frameRequest: number;
+    private water: DetailedSquare;
 }
