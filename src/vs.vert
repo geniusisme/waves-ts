63236d7f409fec9bd289f244f36dff1302b1250f
Eugeen Sablin <EugeenSablin@gmail.com>
2017-12-19 22:07:24 +0300

    ripples as uniforms

--- src/vs.vert
+++ src/vs.vert
@@ -1,32 +1,30 @@
+const float pi = 3.1415926;
+const int max_ripples = 8;
+
 attribute vec3 coordinates;
+
 uniform float time;
+uniform float amplitudes[max_ripples];
+uniform vec3 centers[max_ripples];
+uniform float starts[max_ripples];
 
 varying vec3 position;
 
-const float PI = 3.1415926;
-
-struct Ripple {
-    vec3 center;
-    float start_time;
-};
-
-const Ripple r = Ripple( vec3(0.6, 0.6, 0.0), 0.0 );
-
-float get_z(vec3 pos, Ripple ripple) {
+float get_z(vec3 pos) {
     const float wave_length = 0.3; // units
-    const float dist_kf = 2.0 * PI / wave_length;
+    const float dist_kf = 2.0 * pi / wave_length;
     const float wave_speed = 0.3; // units per sec
-    const float time_kf = -2.0 * PI / (wave_length / wave_speed);
+    const float time_kf = -2.0 * pi / (wave_length / wave_speed);
     const float to_seconds = 0.001;
 
-    float dist = length(ripple.center - pos);
-    float time = to_seconds * (time - ripple.start_time);
+    float dist = length(centers[0] - pos);
+    float time = to_seconds * (time - starts[0]);
 
-    return sin(dist_kf * dist + time_kf * time );
+    return amplitudes[0] * sin(dist_kf * dist + time_kf * time );
 }
 
 void main(void) {
-    position = vec3(coordinates.x, coordinates.y, get_z(coordinates, r));
+    position = vec3(coordinates.x, coordinates.y, get_z(coordinates));
     gl_Position = vec4(coordinates, 1.0);
 }
 
--- src/waves_effect.ts
+++ src/waves_effect.ts
@@ -1,9 +1,16 @@
 import * as vs from './vs.vert';
 import * as fs from './fs.frag';
 
+const max_ripples = 8;
+
 export class WavesEffect {
     constructor(gl: WebGLRenderingContext) {
         this.gl = gl;
+        this.setup_program();
+        this.setup_initial_ripples();
+    }
+
+    setup_program() {
         let vertShader = this.gl.createShader(this.gl.VERTEX_SHADER);
         this.gl.shaderSource(vertShader, vs);
         this.gl.compileShader(vertShader);
@@ -19,8 +26,23 @@ export class WavesEffect {
 
         this.coord_attrib = this.gl.getAttribLocation(program, "coordinates");
         this.time_uniform = this.gl.getUniformLocation(program, "time")!;
+        this.amplitudes_uniform = this.gl.getUniformLocation(program, "amplitudes")!;
+        this.centers_uniform = this.gl.getUniformLocation(program, "centers")!;
+        this.starts_uniform = this.gl.getUniformLocation(program, "starts")!;
 
         this.program = program;
+    }
+
+    setup_initial_ripples() {
+        this.amplitudes = new Float32Array(max_ripples);
+        this.centers = new Float32Array(max_ripples * 3);
+        this.starts = new Float32Array(max_ripples);
+
+        this.amplitudes[0] = 1.0;
+        this.starts[0] = 0.0;
+        this.centers[0] = 0.55;
+        this.centers[1] = 0.55;
+        this.centers[2] = 0.00;
     }
 
     set_time(time: number) {
@@ -32,11 +54,21 @@ export class WavesEffect {
         this.gl.vertexAttribPointer(this.coord_attrib, 3, this.gl.FLOAT, false, 0, 0);
         this.gl.enableVertexAttribArray(this.coord_attrib);
         this.gl.uniform1f(this.time_uniform, this.time);
+        this.gl.uniform1fv(this.amplitudes_uniform, this.amplitudes);
+        this.gl.uniform3fv(this.centers_uniform, this.centers);
+        this.gl.uniform1fv(this.starts_uniform, this.starts);
     }
 
     private time: number;
     private coord_attrib: GLuint;
     private time_uniform: WebGLUniformLocation;
+    private amplitudes_uniform: WebGLUniformLocation;
+    private centers_uniform: WebGLUniformLocation;
+    private starts_uniform: WebGLUniformLocation;
     private program: WebGLProgram;
     private gl: WebGLRenderingContext;
+
+    private amplitudes: Float32Array;
+    private centers: Float32Array;
+    private starts: Float32Array;
 }
