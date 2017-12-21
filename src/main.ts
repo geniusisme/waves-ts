b70f86ec9a67dc72b7693cc44302113de288f1ea
Eugeen Sablin <EugeenSablin@gmail.com>
2017-12-21 20:19:58 +0300

    make ripples on clicks

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,13 @@ import {WavesEffect} from "./waves_effect"
 
 
 window.onload = () => {
-    let app = new Application(<HTMLCanvasElement> document.getElementById('my_Canvas'))
+    let app = new Application(document.getElementById('my_Canvas') as HTMLCanvasElement)
 }
 
 class Application {
     constructor(canvas: HTMLCanvasElement) {
         this.canvas = canvas
-        this.water = new DetailedSquare(0.3, 100)
+        this.water = new DetailedSquare(0.99, 255)
         this.setup_gl_resources()
         this.start_rendering()
         this.setup_listeners()
@@ -52,15 +52,57 @@ class Application {
     }
 
     private setup_listeners() {
-        this.canvas.addEventListener("webglcontextlost", (event: any) => {
+        this.canvas.addEventListener("webglcontextlost", (event: Event) => {
             event.preventDefault()
             window.cancelAnimationFrame(this.frameRequest)
         }, false)
 
-        this.canvas.addEventListener("webglcontextrestored", (event: any) => {
+        this.canvas.addEventListener("webglcontextrestored", () => {
             this.setup_gl_resources()
             this.start_rendering()
         }, false)
+
+        this.canvas.addEventListener("mousedown", () => {
+            this.mouse_downed_at = performance.now()
+        })
+
+        this.canvas.addEventListener("mouseleave", () => {
+            this.mouse_downed_at = undefined
+        })
+
+        this.canvas.addEventListener("mouseup", (event: Event) => {
+            let m_event = event as MouseEvent;
+            this.mouse_up(m_event.clientX, m_event.clientY);
+        })
+    }
+
+    private mouse_up(x: number, y: number) {
+        if (!this.mouse_downed_at) {
+            return
+        }
+        let now = performance.now()
+        let elapsed = now - this.mouse_downed_at
+        this.mouse_downed_at = undefined
+        this.waves_effect.add_ripple(
+            this.time_to_wave_strengh(elapsed),
+            this.mouse_to_world(x, y),
+            now
+        )
+    }
+
+    private time_to_wave_strengh(time: number) {
+        let strength = time / 1000.0 + 0.2
+        return strength > 1.0? 1.0: strength;
+    }
+
+    private mouse_to_world(x: number, y: number): [number, number, number] {
+        let canvas_r = this.canvas.getBoundingClientRect()
+        let canvas_x_y = [x - canvas_r.left, y - canvas_r.top]
+        let screen_x_y = [
+            canvas_x_y[0] * 2.0 / canvas_r.width - 1.0,
+            canvas_x_y[1] * -2.0 / canvas_r.height + 1.0]
+
+        return [screen_x_y[0], screen_x_y[1], 0]
     }
 
     private canvas: HTMLCanvasElement
@@ -70,4 +112,5 @@ class Application {
     private frameRequest: number
     private water: DetailedSquare
     private waves_effect: WavesEffect
+    private mouse_downed_at?: number
 }
